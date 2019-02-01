7f84f2a638d55b8cc65887bbfe350735b177d635
Hans <hans.steffens@gmail.com>
2019-01-31 20:19:21 -0500

    ACF fields in PHP

--- wp-content/themes/codersclan/app/setup.php
+++ wp-content/themes/codersclan/app/setup.php
@@ -35,12 +35,13 @@ function theme_version()
 
     return $version;
 }
+
 /**
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
     $version = theme_version();
-    wp_enqueue_style('font-awesome', '/assets/static/font-awesome.min.css', false, $version);
+    wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/static/font-awesome.min.css', false, $version);
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, $version);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], $version, true);
 
@@ -192,7 +193,7 @@ add_action('acf/init', function () {
 
 /**
  * Only show Custom Fields admin page on ".local" address.
- */
+ *
 add_filter('acf/settings/show_admin', function () {
     $site_url = get_bloginfo('url');
 
@@ -201,34 +202,358 @@ add_filter('acf/settings/show_admin', function () {
     } else {
         return false;
     }
-});
-
+});*/
 
 /**
- * 
+ * Challenge: Create a CPT with a function to replace strings based on time
  */
-
 add_action('init',function(){
 	$args = array(
-      'public' => true,
-      'label'  => 'TimePost'
+    'labels' => array(
+              'name' => __( 'Time Posts' ),
+              'all_items'=> __( 'All Time Posts' ),
+              'singular_name' => __( 'TimePost' )
+          ),
+    'public' => true,
     );
     register_post_type( 'timepost', $args );
-    	$args = array(
+
+    $args = array(
+      'labels' => array(
+                'name' => __( 'Time Posts 2' ),
+                'all_items'=> __( 'All Time Posts 2' ),
+                'singular_name' => __( 'TimePost2' )
+            ),
       'public' => true,
-      'label'  => 'TimePost2'
     );
     register_post_type( 'timepost2', $args );
 });
 
-
-
 add_filter('the_content', function( $content ){
 	global $post;
 
-	if($post->post_type == 'timepost2' && date('H') < 11)
-    	return str_ireplace('Hello', 'Good Morning!', $content);
-    else
-    	return $content;
+	if($post->post_type == 'timepost' && current_time('G') < 11) {
+    return str_ireplace('Hello', 'Good Morning!', $content);
+  } else {
+    return $content;
+  }
+});
+/**
+ * Challenge: Create an ACF Alert section
+ */
 
-} );
+if( function_exists('acf_add_local_field_group') ):
+
+  //Section: Alerts
+  acf_add_local_field_group(array(
+   'key' => 'group_5c534b8ceaade',
+   'title' => 'Section: Alerts',
+   'fields' => array(
+     array(
+       'key' => 'field_5c534bea44eee',
+       'label' => 'Select Alert',
+       'name' => 'select_alert',
+       'type' => 'radio',
+       'instructions' => 'Select an alert type to display in homepage.',
+       'required' => 0,
+       'conditional_logic' => 0,
+       'wrapper' => array(
+       'width' => '',
+       'class' => '',
+       'id' => '',
+       ),
+       'choices' => array(
+         'warning' => 'Warning',
+         'success' => 'Success',
+         'error' => 'Error',
+       ),
+       'allow_null' => 0,
+       'other_choice' => 0,
+       'default_value' => '',
+       'layout' => 'vertical',
+       'return_format' => 'value',
+       'save_other_choice' => 0,
+     ),
+     array(
+       'key' => 'field_5c534bc944eed',
+       'label' => 'Success Alert',
+       'name' => 'success_msg',
+       'type' => 'text',
+       'instructions' => 'Insert success message here.',
+       'required' => 0,
+       'conditional_logic' => array(
+         array(
+           array(
+             'field' => 'field_5c534bea44eee',
+             'operator' => '==',
+             'value' => 'success',
+           ),
+         ),
+       ),
+       'wrapper' => array(
+         'width' => '',
+         'class' => '',
+         'id' => '',
+       ),
+       'default_value' => '',
+       'placeholder' => '',
+       'prepend' => '',
+       'append' => '',
+       'maxlength' => '',
+     ),
+     array(
+       'key' => 'field_5c534c6f44eef',
+       'label' => 'Warning Alert',
+       'name' => 'warning_msg',
+       'type' => 'text',
+       'instructions' => 'Insert warning message here.',
+       'required' => 0,
+       'conditional_logic' => array(
+         array(
+           array(
+             'field' => 'field_5c534bea44eee',
+             'operator' => '==',
+             'value' => 'warning',
+           ),
+         ),
+       ),
+       'wrapper' => array(
+         'width' => '',
+         'class' => '',
+         'id' => '',
+       ),
+       'default_value' => '',
+       'placeholder' => '',
+       'prepend' => '',
+       'append' => '',
+       'maxlength' => '',
+     ),
+     array(
+       'key' => 'field_5c534ce044ef0',
+       'label' => 'Error Alert',
+       'name' => 'error_msg',
+       'type' => 'text',
+       'instructions' => 'Insert error message here.',
+       'required' => 0,
+       'conditional_logic' => array(
+         array(
+           array(
+             'field' => 'field_5c534bea44eee',
+             'operator' => '==',
+             'value' => 'error',
+           ),
+         ),
+       ),
+       'wrapper' => array(
+         'width' => '',
+         'class' => '',
+         'id' => '',
+       ),
+       'default_value' => '',
+       'placeholder' => '',
+       'prepend' => '',
+       'append' => '',
+       'maxlength' => '',
+     ),
+   ),
+   'location' => array(
+     array(
+       array(
+         'param' => 'post_type',
+         'operator' => '==',
+         'value' => 'post',
+       ),
+     ),
+   ),
+   'menu_order' => 0,
+   'position' => 'normal',
+   'style' => 'default',
+   'label_placement' => 'top',
+   'instruction_placement' => 'label',
+   'hide_on_screen' => '',
+   'active' => 0,
+   'description' => '',
+  ));
+
+  //Partial: Sections
+  acf_add_local_field_group(array(
+   'key' => 'group_5badfc735aa1f',
+   'title' => 'Partial: Sections',
+   'fields' => array(
+     array(
+       'key' => 'field_5badfc78b1465',
+       'label' => 'Sections',
+       'name' => 'sections',
+       'type' => 'flexible_content',
+       'instructions' => '',
+       'required' => 0,
+       'conditional_logic' => 0,
+       'wrapper' => array(
+         'width' => '',
+         'class' => '',
+         'id' => '',
+       ),
+       'layouts' => array(
+         '5badfc7de5e91' => array(
+           'key' => '5badfc7de5e91',
+           'name' => 'hello_world',
+           'label' => 'Hello World',
+           'display' => 'block',
+           'sub_fields' => array(
+             array(
+               'key' => 'field_5badfc87b1466',
+               'label' => 'Section: Hello World',
+               'name' => 'section:_hello_world',
+               'type' => 'clone',
+               'instructions' => '',
+               'required' => 0,
+               'conditional_logic' => 0,
+               'wrapper' => array(
+                 'width' => '',
+                 'class' => '',
+                 'id' => '',
+               ),
+               'clone' => array(
+                 0 => 'group_5badfc361d354',
+               ),
+               'display' => 'seamless',
+               'layout' => 'block',
+               'prefix_label' => 0,
+               'prefix_name' => 0,
+             ),
+           ),
+           'min' => '',
+           'max' => '',
+         ),
+         'layout_5bc49a98959b1' => array(
+           'key' => 'layout_5bc49a98959b1',
+           'name' => 'broken_sidebar',
+           'label' => 'Broken Sidebar',
+           'display' => 'block',
+           'sub_fields' => array(
+             array(
+               'key' => 'field_5bc49a98959b2',
+               'label' => 'Section: Broken Sidebar',
+               'name' => 'section:_broken_sidebar',
+               'type' => 'clone',
+               'instructions' => '',
+               'required' => 0,
+               'conditional_logic' => 0,
+               'wrapper' => array(
+                 'width' => '',
+                 'class' => '',
+                 'id' => '',
+               ),
+               'clone' => array(
+                 0 => 'group_5bc49ab339a7b',
+               ),
+               'display' => 'seamless',
+               'layout' => 'block',
+               'prefix_label' => 0,
+               'prefix_name' => 0,
+             ),
+           ),
+           'min' => '',
+           'max' => '',
+         ),
+         'layout_5bc49f364b3e0' => array(
+           'key' => 'layout_5bc49f364b3e0',
+           'name' => 'social_icons',
+           'label' => 'Social Icons',
+           'display' => 'block',
+           'sub_fields' => array(
+             array(
+               'key' => 'field_5bc49f364b3e1',
+               'label' => 'Section: Social Icons',
+               'name' => 'section:_social_icons',
+               'type' => 'clone',
+               'instructions' => '',
+               'required' => 0,
+               'conditional_logic' => 0,
+               'wrapper' => array(
+                 'width' => '',
+                 'class' => '',
+                 'id' => '',
+               ),
+               'clone' => array(
+                 0 => 'group_5bc49f199d19f',
+               ),
+               'display' => 'seamless',
+               'layout' => 'block',
+               'prefix_label' => 0,
+               'prefix_name' => 0,
+             ),
+           ),
+           'min' => '',
+           'max' => '',
+         ),
+         'layout_5c5379b4dc4fd' => array(
+           'key' => 'layout_5c5379b4dc4fd',
+           'name' => 'alerts',
+           'label' => 'Alerts',
+           'display' => 'block',
+           'sub_fields' => array(
+             array(
+               'key' => 'field_5c537a0edc4ff',
+               'label' => 'Section: Alerts',
+               'name' => 'section:_alerts',
+               'type' => 'clone',
+               'instructions' => '',
+               'required' => 0,
+               'conditional_logic' => 0,
+               'wrapper' => array(
+                 'width' => '',
+                 'class' => '',
+                 'id' => '',
+               ),
+               'clone' => array(
+                 0 => 'group_5c534b8ceaade',
+               ),
+               'display' => 'seamless',
+               'layout' => 'block',
+               'prefix_label' => 0,
+               'prefix_name' => 0,
+             ),
+           ),
+           'min' => '',
+           'max' => '',
+         ),
+       ),
+       'button_label' => 'Add Row',
+       'min' => '',
+       'max' => '',
+     ),
+   ),
+   'location' => array(
+     array(
+       array(
+         'param' => 'post_type',
+         'operator' => '==',
+         'value' => 'post',
+       ),
+     ),
+   ),
+   'menu_order' => 0,
+   'position' => 'normal',
+   'style' => 'default',
+   'label_placement' => 'top',
+   'instruction_placement' => 'label',
+   'hide_on_screen' => '',
+   'active' => 0,
+   'description' => '',
+  ));
+
+endif;
+
+/**
+ * Challenge: Local Jason setup
+*/
+add_filter('acf/settings/save_json', function ( $path ) {
+
+    // update path
+    $path = get_stylesheet_directory() . '/acf-json';
+
+    // return
+    return $path;
+
+});
---
+++ wp-content/themes/codersclan/resources/views/sections/alerts.blade.php
@@ -0,0 +1,7 @@
+<section class="section section-alerts">
+    <div class="container">
+
+       <p>@php echo get_field('error_msg') @endphp</p>
+
+    </div>
+</section>
